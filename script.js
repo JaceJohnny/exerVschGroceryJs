cbd7c7a6639da937a29970572de0d9259a68ea2f
JaceJohnny <jace.johnny@hci.utah.edu>
2021-11-16 19:27:30 -0700

    completed Grocery Store assignment

---
+++ script.js
@@ -0,0 +1,13 @@
+var shopper = {
+    StoreNames: "Harmons", //string
+    ZipCodes: 84106, //number
+    IsLocal: true, //boolean
+    grocerycart : ["Bell peppers", "Tomatoes", "chicken", "rice", "cheese"]
+}
+
+
+
+
+console.log(shopper)
+//include each of the following: string, number, boolean, function
+//should add a grocery cart property to object with array of items in grocery cart
